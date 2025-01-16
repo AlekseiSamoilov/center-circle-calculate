ef6fc760a695d63ca79350da1731fc0d2b51833f
Aleksei Samoilov <lek-z@yandex.ru>
2025-01-16 22:39:32 +0100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/center-circle-calculate/sw.js', { scope: '/center-circle-calculate/' })})}
